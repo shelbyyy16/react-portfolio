eb910f684154c0de08dab46d5b033f854ef611d0
honeybee <shelby.e.pagan@gmail.com>
2023-12-11 15:57:29 -0500

    stub out About page andprojects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,43 @@
-function Projects(props) {
-    return <h1>Projects</h1>;
-  }
+import { useState, useEffect } from "react";
+
+function Projects() {
+
+  const [projects, setProjects] = useState(null);
+
+
+  const getProjectsData = async () => {
+
+
+    const response = await fetch("./projects.json");
+
+
+    const data = await response.json();
+
+
+    setProjects(data);
+
+  };
+
   
-  export default Projects;
+  useEffect(() => getProjectsData(), []);
+
+
+  const loaded = () => {
+    return projects.map((project) => (
+      <div>
+        <h1>{project.name}</h1>
+        <img src={project.image} />
+        <a href={project.git}>
+          <button>Github</button>
+        </a>
+        <a href={project.live}>
+          <button>live site</button>
+        </a>
+      </div>
+    ));
+  };
+
+  return projects ? loaded() : <h1>Loading...</h1>;
+}
+
+export default Projects;
