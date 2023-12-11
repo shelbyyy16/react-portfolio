795927112022be49919fef2d2654ac33bbc8c996
honeybee <shelby.e.pagan@gmail.com>
2023-12-11 15:41:21 -0500

    stub out App.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,32 @@
-function Header(props) {
-    return <h1>Header</h1>;
-  }
+import { Link } from "react-router-dom";
+
+function Header() {
+
+  const navStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    borderBottom: ".2rem solid black",
+    padding: ".5rem",
+    width: "90%",
+    margin: "auto",
+  };
   
-  export default Header;
+  return (
+    <header>
+      <h1>My Portfolio Page</h1>
+      <nav style={navStyle}>
+        <Link to="/">
+          <div>HOME</div>
+        </Link>
+        <Link to="/about">
+          <div>ABOUT</div>
+        </Link>
+        <Link to="/projects">
+          <div>PROJECTS</div>
+        </Link>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
