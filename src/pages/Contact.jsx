8eaa332f24034f4f4d45871392f8b063ed098759
honeybee <shelby.e.pagan@gmail.com>
2023-12-14 16:09:48 -0500

    css for contact me buttons

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,16 @@
+import React from 'react';
+
 function Contact() {
-    return (
-      <div className="contact">
-    <h2>Let's chat.</h2>
-    <div className="home-about">
-    <p>Email: Shelby.E.Pagan@gmail.com</p>
-    <p>LinkedIn: https://www.linkedin.com/in/shelbypagan/</p>
-    <p>Github: https://github.com/shelbyyy16</p>
-    </div></div>
-    )
-  }
-  
-  export default Contact;
+  return (
+    <div className="contact">
+      <h2>Let's chat.</h2>
+      <div className="home-about">
+        <p>Email: Shelby.E.Pagan@gmail.com</p>
+        <div className='contact-buttons-container'><a href="https://www.linkedin.com/in/shelbypagan/" target="_blank" rel="noopener noreferrer" className='contact-button'>LinkedIn</a>
+        <a href="https://github.com/shelbyyy16" target="_blank" rel="noopener noreferrer" className='contact-button'>Github</a></div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 function Home() {
-    return (
-      <div className="home">
-    <p>Hi, my name is</p>
-    <h1>Shelby Pagan.</h1>
-    <h2>Welcome to my corner of the internet.</h2>
-    <div className="home-about">
-    <p>Full Stack Developer and former critical care nurse with a strong track record in high-pressure environments.
-    Proficient in frontend and backend development, with a keen eye for detail and a commitment to delivering<span className="highlight"> high-quality code.</span> </p>
-    </div></div>
-    )
-  }
-  
-  export default Home;
+  return (
+    <div className="home">
+  <p>Hi, my name is</p>
+  <h1>Shelby Pagan.</h1>
+  <h2>Welcome to my corner of the internet.</h2>
+  <div className="home-about">
+  <p>Full Stack Developer and former critical care nurse with a strong track record in high-pressure environments.
+  Proficient in frontend and backend development, with a keen eye for detail and a commitment to delivering<span className="highlight"> high-quality code.</span> </p>
+  </div></div>
+  )
+}
+
+export default Home;
