4c7fbafcd4c247f2da0611cced0fb6f6fe5bb4a1
honeybee <shelby.e.pagan@gmail.com>
2023-12-14 16:21:56 -0500

    merge with main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter as BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
