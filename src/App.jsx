3ce2738ae43619725254afe6f81c2193d34d20d9
honeybee <shelby.e.pagan@gmail.com>
2023-12-21 11:05:01 -0500

    update resume. add smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -8,6 +8,19 @@ import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 function App() {
+  useEffect(() => {
+
+    const links = document.querySelectorAll(".NAV div");
+
+    links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const targetId = e.target.textContent.toLowerCase();
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: "smooth" });
+      });
+    });
+  }, []);
   return (
     <div className="App">
       <Header />
