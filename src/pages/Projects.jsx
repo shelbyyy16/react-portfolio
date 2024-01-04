f43c29bfe4cfaeecd706100926d47aa8815c5b87
honeybee <shelby.e.pagan@gmail.com>
2024-01-04 16:31:46 -0500

    update about me and create projects carousel

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,6 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { useState, useEffect } from "react";
 
 function Projects() {
@@ -25,20 +28,36 @@ function Projects() {
 
 
   const loaded = () => {
-    return (projects.map((project) => (
-      <div id="projects" key={project.id} className="projects">
-        <h2>{project.name}</h2>
-        <img src={project.image} alt={project.name} className="projects-img"/>
-        <br></br>
-        <p>{project.description}</p>
-        <div className="button-container"><a href={project.git}>
-          <button>Github</button>
-        </a><a href={project.live}>
-          <button>Live Site</button>
-        </a></div>
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+  
+    return (
+      <div className="carousel-container"> 
+        <Slider {...settings}>
+          {projects.map((project) => (
+            <div key={project.id} className="projects">
+              <h2>{project.name}</h2>
+              <img src={project.image} alt={project.name} className="projects-img" />
+              <br></br>
+              <p>{project.description}</p>
+              <div className="button-container">
+                <a href={project.git}>
+                  <button>Github</button>
+                </a>
+                <a href={project.live}>
+                  <button>Live Site</button>
+                </a>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
-    
-    )));
+    );
   };
 
   return projects ? loaded() : <h1></h1>;
