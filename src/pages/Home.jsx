b3f108cfd11933b88b2035b977e716bfe11e958b
honeybee <shelby.e.pagan@gmail.com>
2024-01-24 10:03:17 -0500

    add bootstrap CDN. Work on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,19 @@
 function Home() {
   return (
     <div className="home">
-  <p>Hi, my name is</p>
-  <h1>Shelby Pagan.</h1>
-  <h2>Welcome to my corner of the internet.</h2>
-  <div className="home-about">
-  <p>Full Stack Developer and former critical care nurse with a strong track record in high-pressure environments.
-  Proficient in frontend and backend development, with a keen eye for detail and a commitment to delivering<span className="highlight"> high-quality code.</span> </p>
-  </div></div>
-  )
+      <div className="intro">
+      <p>Hi, my name is</p>
+      <h1>Shelby Pagan.</h1>
+      <h2>Welcome to my corner of the internet.</h2>
+      </div>
+      <div className="about-me">
+        <h1>About Me</h1>
+      <p>
+      I have a unique journey that led me from the fast-paced world of critical care nursing to the dynamic field of software engineering. With a background in both Emergency Room (ER) and Cardiac nursing, I honed my skills in high-stakes, life-saving environments. I developed a passion for <span className="highlight">problem-solving, attention to detail, and the ability to stay calm under pressure.</span> These qualities have been invaluable in my transition to the tech industry. The turning point in my journey was when I discovered the power of technology to enhance the patient experience and improve healthcare systems. I served as the Epic Systems Trainer to my unit where I was drawn to the creative and problem-solving aspects of software development and web design, which led me to take the leap and pursue a Full Stack Software Engineering education at General Assembly. My journey is a testament to the idea that career transitions are possible with determination and a commitment to learning. I'm excited to be a part of the ever-evolving tech world, where I can leverage my diverse experiences to <span className="highlight">create solutions that matter.</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
